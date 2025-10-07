95c928caf359bc9a480782a65344a5a99c5b428c
MerigoDavi <davi.ferreira@sou.inteli.edu.br>
2025-10-07 17:17:09 -0300

    Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
-// vite.config.ts
-import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import tailwindcss from '@tailwindcss/vite' // Adicionado no passo anterior
+import tailwindcss from '@tailwindcss/vite' 
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()], // tailwindcss() já deve estar aqui
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  base: '/calculadora-investimentos/', 
+  plugins: [react(), tailwindcss()],
 })
